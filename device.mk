d379108bcc7e37bb6a570d0391a90f8f1b8e5faf
weivincewang <wvw@google.com>
2019-03-29 00:23:41 -0400

    mido: switch to libperfmgr for powerhint
    Bug: 129495482
    Bug: 111098032
    Test: Boot and check PowerHAL
    Change-Id: I79b7276ff0c28885877af8b0533fe1f9d81b3b0c

--- device.mk
+++ device.mk
@@ -292,10 +292,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.harrdware.power@1.3-service.sm6150-libperfmgr \
+    powerhint.json
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # QMI
