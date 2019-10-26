c6f7820012daac3c52f35f3b8baaf52be972f85d
rajadeja <raj.jdja@gmail.com>
2019-10-26 07:33:35 +0000

    mido: Add AuthSecret interface
    Signed-off-by: AnkitGourav <Ankitgourav035@gmail.com>

--- device.mk
+++ device.mk
@@ -123,6 +123,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
+# AuthSecret 
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec \
