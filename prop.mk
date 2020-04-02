eb9a6ef1f5630757afe20ec0b576e5cd4f4a9be3
Hasaber8 <Hasaber8@users.noreply.github.com>
2020-04-02 15:50:41 +0000

    mido: Add Netflix-Specific prop
    * so it recognizes the device as HD playback eligible.
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- prop.mk
+++ prop.mk
@@ -207,3 +207,7 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# Netflix
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.netflix.bsp_rev=Q660-13149-1
