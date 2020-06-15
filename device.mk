9ea3d345ea42bad462530c03ff4bd50992c3f5ea
Dmitry Shmidt <dimitrysh@google.com>
2020-06-15 15:59:53 -0700

    mido: Switch drm HAL to version 1.3
    Bug: 168066487
    Test: Manual
    
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>
    Change-Id: I4eb09fd2403d9366ff775aee0afefef6783f3d41

--- device.mk
+++ device.mk
@@ -190,8 +190,10 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.2-service-lazy.clearkey
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.3-service.widevine \
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
