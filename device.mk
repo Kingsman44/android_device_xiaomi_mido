4ae6c2906a0fc4e7a9e13d97608afe35a8db2bc3
Scott Warner <Tortel1210@gmail.com>
2020-06-24 12:38:56 -0400

    mido: Use opensource btconfigstore
    Change-Id: Id16333c0b36fbf93e504fe6cb0107fce88721791
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- device.mk
+++ device.mk
@@ -126,7 +126,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libldacBT_dec \
-    libbt-vendor
+    libbt-vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
