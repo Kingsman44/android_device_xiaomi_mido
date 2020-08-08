cb68d71101896bf48ef456b656a3929e1e4bf929
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-08-08 10:45:40 +0000

    mido: Switch iwlan operation mode to legacy
    *fix videocall black camera

--- prop.mk
+++ prop.mk
@@ -63,7 +63,7 @@ media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
-persist.vendor.qti.telephony.vt_cam_interface=1 \
+persist.vendor.qti.telephony.vt_cam_interface=2 \
 vidc.enc.dcvs.extra-buff-count=2 \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.motorola.faceunlock
 
@@ -219,6 +219,7 @@ ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,22 \
 ro.telephony.use_old_mnc_mcc_format=true \
 service.qti.ims.enabled=1 \
+ro.telephony.iwlan_operation_mode=legacy \
 vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 telephony.lteOnCdmaDevice=1 \
 persist.sys.fflag.override.settings_network_and_internet_v2=true
