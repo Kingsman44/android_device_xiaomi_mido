673a1dac8ce38d23711b801be71d5eeff7172667
Kingsman44 <shivan.0972@gmail.com>
2020-08-29 06:01:28 +0000

    mido: add product common props
    * Q supports product build.prop check google stock builds they include common build properties in product

--- product_prop.mk
+++ product_prop.mk
@@ -5,6 +5,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Product common build properties
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.product.product.brand=Xiaomi \
+ro.product.product.device=mido \
+ro.product.product.manufacturer=Xiaomi \
+ro.product.product.model=Redmi Note 4 \
+ro.product.product.name=aosp_mido \
+ro.product.build.fingerprint=xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys
+
 # Default to BFQ I/O scheduler
 PRODUCT_PRODUCT_PROPERTIES += \
-    persist.sys.io.scheduler=bfq
+persist.sys.io.scheduler=bfq
